12b5483fdde9a2758e7dae774e7dfd1c21d2d4b7
Henk W. M. Raam <hwmraam@raamwerken.nl>
2014-06-22 14:11:00 +0200

    Add precompiled assets for Heroku

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ SampleApp::Application.routes.draw do
   root  'static_pages#home'
   match '/signup',  to: 'users#new', via: 'get'
   get "static_pages/home"
-  get "static_pages/help"
-  get "static_pages/about"
-  get "static_pages/contact"
+  match '/help',    to: 'static_pages#help',    via: 'get'
+  match '/about',   to: 'static_pages#about',   via: 'get'
+  match '/contact', to: 'static_pages#contact', via: 'get'
 
   #root to: "static_pages#home"
 
